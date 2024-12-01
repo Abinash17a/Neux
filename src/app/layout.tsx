661dd811002427931172f46285813248e9967de0
Abinash17a <77913801+Abinash17a@users.noreply.github.com>
2024-12-01 22:34:46 +0530

    landing page code working and basic layout created

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,13 @@
 import Header from '../app/components/Header';
 import Footer from '../app/components/Footer';
+import './globals.css';
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
       <html lang="en">
-          <body className="bg-black text-white">
+          <body className="bg-black text-white min-h-screen flex flex-col">
               <Header />
-              <main>{children}</main>
+              <main className="flex-grow">{children}</main>
               <Footer />
           </body>
       </html>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,7 +9,7 @@ export default function HomePage() {
                     Your Partner in Building Cutting-Edge Digital Experiences.
                     From web development to scalable solutions, we bring your ideas to life with precision, creativity, and unmatched expertise.
                 </p>
-                <button className="mt-8 bg-blue-500 text-white px-6 py-3 rounded-lg hover:bg-blue-400">
+                <button className="mt-8 bg-blue-500 text-white px-6 py-3 rounded-lg hover:bg-blue-400 shadow-lg hover:shadow-blue-300/50 transition-all duration-100 ease-in-out font-semibold text-lg">
                     Get in Touch
                 </button>
             </section>
